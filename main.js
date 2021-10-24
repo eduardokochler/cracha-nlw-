9d2df8d8f013a81468936d0d02a488bc1faf2a4e
eduardo kochler <92959821+eduardokochler@users.noreply.github.com>
2021-10-24 01:49:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const LinksSocialMedia = {
+  github: 'eduardokochler',
+  youtube: 'eduardokochler',
+  facebook: 'eduardokochler',
+  instagram: 'eduardo_kochler',
+  twitter: 'eduardokochler2'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
